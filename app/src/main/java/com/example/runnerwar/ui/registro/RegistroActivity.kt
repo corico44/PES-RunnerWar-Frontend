30c9f123758c4be221a797e9b9a83396d23f580d
Pau RE <pau.josep.ruiz@estudiantat.upc.udu>
2021-03-19 22:14:42 +0100

    Intento de conectar mainActivity con RegistroActivity

--- app/src/main/java/com/example/runnerwar/ui/registro/RegistroActivity.kt
+++ app/src/main/java/com/example/runnerwar/ui/registro/RegistroActivity.kt
@@ -1,19 +1,25 @@
 package com.example.runnerwar.ui.registro
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.runnerwar.R
 
+
+
 class RegistroActivity : AppCompatActivity() {
     private lateinit var  registroViewModel: RegistroViewModel      //"lateinit" se utiliza para indicar que se inicializará más tarde, dado que no acepta un nul
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContentView(R.layout.registro)
 
@@ -21,12 +27,39 @@ class RegistroActivity : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.reg_email)
         val password = findViewById<EditText>(R.id.reg_password)
         val conditions = findViewById<CheckBox>(R.id.reg_checkbox)
-        val login_button = findViewById<Button>(R.id.login_button)
+        val signup_button = findViewById<Button>(R.id.login_button)
 
-        /*login_button.setOnClickListener{
-            registroViewModel.login(username.text.toString(), email.text.toString(), )
-        }*/
+        registroViewModel.registroFormState.observe(this@RegistroActivity, Observer{
+            val registroForm = it ?: return@Observer
+
+            if(!registroForm.isDataValid)
+                Toast.makeText(applicationContext, registroForm.usernameError, Toast.LENGTH_SHORT).show()
+        })
+
+        username.afterTextChanged {
+            registroViewModel.singUpDataChanged(
+                username.text.toString(),
+                email.text.toString(),
+                password.text.toString()
+            )
+        }
+
+        signup_button.setOnClickListener{
+            registroViewModel.signUp(username.text.toString(), email.text.toString(),password.text.toString() )
+        }
 
 
     }
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
 }
