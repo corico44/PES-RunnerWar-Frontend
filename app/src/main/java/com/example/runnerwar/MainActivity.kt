e40cecadb7b18feb395625dd58a728823222479a
Jose Antonio Giner Muñoz <jaginer@trustedadvisors.tech>
2021-03-20 20:44:22 +0100

    Incluye la nueva pantalla de selección de facción.

--- app/src/main/java/com/example/runnerwar/MainActivity.kt
+++ app/src/main/java/com/example/runnerwar/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.runnerwar.ui.registro.RegistroActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.seleccion_faccion)
 
 
 
