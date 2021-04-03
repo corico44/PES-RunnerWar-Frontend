511810bed9e54bd01b578d36dd3bba75b9bf8f71
Jose Antonio Giner Muñoz <jaginer@trustedadvisors.tech>
2021-04-03 12:32:49 +0200

    Cambios en creacion cuenta para cumplir criterios de aceptación

--- app/src/main/java/com/example/runnerwar/ui/registro/RegistroViewModel.kt
+++ app/src/main/java/com/example/runnerwar/ui/registro/RegistroViewModel.kt
@@ -37,12 +37,18 @@ class RegistroViewModel(private val repository: RegistroRepository) : ViewModel(
         } else if (!isEmailValid(email) ) {
             _registroForm.value = RegistroFormState(emailError = "Not a valid email" )
         } else if (!isPasswordValid(password)) {
-            _registroForm.value = RegistroFormState(passwordError = "Password must be >5 characters" )
+            if(!contieneMinuscula(password)){
+                _registroForm.value = RegistroFormState(passwordError = "Password must contain a lower case")
+            } else if(!contieneMayuscula(password)){
+                _registroForm.value = RegistroFormState(passwordError = "Password must contain a capital letter")
+            } else {
+                _registroForm.value = RegistroFormState(passwordError = "Password must be > 5 characters")
+            }
         } else {
             _registroForm.value = RegistroFormState(isDataValid = true)
         }
     }
-
+    var emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     // A placeholder password validation check
     private fun isUserNameValid(username: String): Boolean {
         return (username.length in 5..10)
@@ -50,16 +56,40 @@ class RegistroViewModel(private val repository: RegistroRepository) : ViewModel(
 
     // A placeholder username validation check
     private fun isEmailValid(email: String): Boolean {
-        return if (!email.contains('@')) {
-            Patterns.EMAIL_ADDRESS.matcher(email).matches()
-        } else {
-            email.isNotBlank()
+        //if (!email.contains('@')) {
+            //Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+        //} else {
+            //return email.isNotBlank()
+        //}
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+
+    private fun contieneMayuscula(a: String): Boolean {
+        for(i in 0..(a.length-1)) {
+            if (a[i] in 'A'..'Z') {
+                return true
+            }
         }
+        return false
+    }
+
+    private fun contieneMinuscula(a: String): Boolean {
+        for(i in 0..(a.length-1)) {
+            if (a[i] in 'a'..'z') {
+                return true
+            }
+        }
+        return false
     }
 
     // A placeholder password validation check
     private fun isPasswordValid(password: String): Boolean {
-        return (password.length in 6..12)
+        if(password.contains('!') or password.contains('_') or password.contains('/')){
+            return false
+        }
+        return (password.length in 6..12 && contieneMayuscula(password) && contieneMinuscula(password))
     }
 
 }
