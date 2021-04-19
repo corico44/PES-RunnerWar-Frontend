c80134a7265914d94bed5274977d588e77ffd6b2
Cori <cairigaray999@gmail.com>
2021-04-19 21:14:00 +0200

    consultar-mapa: mapa integrado correctamente

--- app/src/main/java/com/example/runnerwar/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/example/runnerwar/ui/maps/MapsActivity.kt
@@ -1,18 +1,13 @@
-package com.example.runnerwar.ui.mapa
+package com.example.runnerwar.ui.maps
 
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.FragmentActivity
+import com.example.runnerwar.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -21,42 +16,25 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import android.R
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 
-class MapaFragment : Fragment(), OnMapReadyCallback {
-
-    private lateinit var mapaViewModel: MapaViewModel
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var currentLocation: Location
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mapaViewModel =
-            ViewModelProviders.of(this).get(MapaViewModel::class.java)
-        val root = inflater.inflate(com.example.runnerwar.R.layout.fragment_mapa, container, false)
-
-        return root
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view,savedInstanceState)
-        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(activity!!)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this@MapsActivity)
         fetchLocation()
     }
-
     private fun fetchLocation() {
         if (ActivityCompat.checkSelfPermission(
-                activity!!, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                this, Manifest.permission.ACCESS_FINE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-             activity!!, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                activity!!,
+            ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
             return
         }
@@ -64,11 +42,11 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
         task.addOnSuccessListener { location ->
             if (location != null) {
                 currentLocation = location
-                Toast.makeText(activity?.application!!, currentLocation.latitude.toString() + "" +
+                Toast.makeText(applicationContext, currentLocation.latitude.toString() + "" +
                         currentLocation.longitude, Toast.LENGTH_SHORT).show()
-                val mapFragment = childFragmentManager.findFragmentById(com.example.runnerwar.R.id.map) as SupportMapFragment?
-
-                mapFragment!!.getMapAsync(this)
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.map) as
+                        SupportMapFragment?)!!
+                supportMapFragment.getMapAsync(this@MapsActivity)
             }
         }
     }
@@ -77,16 +55,15 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
         val markerOptions = MarkerOptions().position(latLng).title("I am here!")
         googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
         googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow_down_float))
         googleMap?.addMarker(markerOptions)
     }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
                                             grantResults: IntArray) {
         when (requestCode) {
             permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] ==
-                PackageManager.PERMISSION_GRANTED) {
-                fetchLocation()
-            }
+            PackageManager.PERMISSION_GRANTED) {
+            fetchLocation()
+        }
         }
     }
 }
