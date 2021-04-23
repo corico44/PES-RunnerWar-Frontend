f9fdbe56cf3f02997a14d1d02dff7ffb56361fad
Cori <cairigaray999@gmail.com>
2021-04-23 19:48:16 +0200

    consultar-mapa: ubicacion del usuario se actualiza

--- app/src/main/java/com/example/runnerwar/ui/mapa/MapaFragment.kt
+++ app/src/main/java/com/example/runnerwar/ui/mapa/MapaFragment.kt
@@ -13,8 +13,6 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -22,15 +20,37 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import android.R
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.location.LocationManager
+import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import android.hardware.SensorManager
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.common.api.GoogleApiClient
+
+
+
+
+
+
 
 
 class MapaFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapaViewModel: MapaViewModel
     private lateinit var currentLocation: Location
+    private var lastLocation: Location = Location("dummyprovider");
+    private var lastMarker: Marker? = null
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
+    // globally declare LocationRequest
+    private lateinit var locationRequest: LocationRequest
+    private var mapFragment: SupportMapFragment? = null
+
+    // globally declare LocationCallback
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,40 +66,119 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view,savedInstanceState)
         fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(activity!!)
-        fetchLocation()
+        var locationManager = activity!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        var mGoogleApiClient: GoogleApiClient? = null
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect()
+        }
+
+        getLocationUpdates()
     }
 
-    private fun fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                activity!!, Manifest.permission.ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-             activity!!, Manifest.permission.ACCESS_COARSE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                activity!!,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
-            return
-        }
-        val task = fusedLocationProviderClient.lastLocation
-        task.addOnSuccessListener { location ->
-            if (location != null) {
-                currentLocation = location
-                Toast.makeText(activity?.application!!, currentLocation.latitude.toString() + "" +
-                        currentLocation.longitude, Toast.LENGTH_SHORT).show()
-                val mapFragment = childFragmentManager.findFragmentById(com.example.runnerwar.R.id.map) as SupportMapFragment?
+    // A REVISAR
+    private fun fetchLocation(currentLocation: Location) {
 
-                mapFragment!!.getMapAsync(this)
+        Toast.makeText(activity?.application!!, currentLocation.latitude.toString() + "" +
+                currentLocation.longitude, Toast.LENGTH_SHORT).show()
+        val mapFragment = childFragmentManager.findFragmentById(com.example.runnerwar.R.id.map) as SupportMapFragment?
+        println("antes del mapa")
+        mapFragment!!.getMapAsync(this)
+
+
+    }
+
+    /**
+     * call this method in onCreate
+     * onLocationResult call when location is changed
+     */
+    private fun getLocationUpdates()
+    {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 5000
+        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+
+                if (locationResult.locations.isNotEmpty()) {
+                    // get latest location
+                    val location =
+                        locationResult.lastLocation
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    // Aquí tratar los valores para ponerlos como current y actualizar el mapa
+                    if (location != null) {
+                        currentLocation = location
+                        fetchLocation(currentLocation)
+                        println("entro")
+                        // fetchLocation()
+                    }
+                }
+
             }
         }
     }
-    override fun onMapReady(googleMap: GoogleMap?) {
+
+    //start location updates
+    private fun startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    // stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    // stop receiving location update when activity not visible/foreground
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    // start receiving location update when activity  visible/foreground
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+
+   override fun onMapReady(googleMap: GoogleMap?) {
+       if(lastLocation != null) {
+               if((!lastLocation.latitude.equals(currentLocation.latitude)) ||
+                   (!lastLocation.longitude.equals(currentLocation.longitude)))
+               {
+
+                   if(lastMarker != null){
+                       println("entro remove")
+                       println(lastMarker)
+                       lastMarker!!.remove()
+
+                   }
+
+               }
+       }
+
         val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
         val markerOptions = MarkerOptions().position(latLng).title("I am here!")
         googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
         googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
         markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow_down_float))
-        googleMap?.addMarker(markerOptions)
+
+        lastLocation = currentLocation
+
+       lastMarker = googleMap?.addMarker(markerOptions)
     }
+
+
+    /*
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
                                             grantResults: IntArray) {
         when (requestCode) {
@@ -88,5 +187,5 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
                 fetchLocation()
             }
         }
-    }
+    }*/
 }
