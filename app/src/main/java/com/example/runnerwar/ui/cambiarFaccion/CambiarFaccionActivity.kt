67561157dde8531f26b379f096276cebcf674100
Cori <cairigaray999@gmail.com>
2021-06-02 21:48:51 +0200

    master: añadido idioma en cambio faccion

--- app/src/main/java/com/example/runnerwar/ui/cambiarFaccion/CambiarFaccionActivity.kt
+++ app/src/main/java/com/example/runnerwar/ui/cambiarFaccion/CambiarFaccionActivity.kt
@@ -2,6 +2,7 @@ package com.example.runnerwar.ui.cambiarFaccion
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import com.example.runnerwar.Model.FactionForm
 import com.example.runnerwar.NavActivity
 import com.example.runnerwar.R
 import com.example.runnerwar.Repositories.UserRepository
+import com.example.runnerwar.util.Language
 import com.example.runnerwar.util.Session
 import kotlinx.android.synthetic.main.activity_cambiar_faccion.imageButton2
 import kotlinx.android.synthetic.main.activity_cambiar_faccion.imageButton4
@@ -27,12 +29,32 @@ class CambiarFaccionActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cambiar_faccion)
 
+        val titulo = findViewById<TextView>(R.id.titulo)
+        val texto_amarillo = findViewById<TextView>(R.id.texto_amarillo)
+        val texto_rojo = findViewById<TextView>(R.id.texto_rojo)
+        val texto_azul = findViewById<TextView>(R.id.texto_azul)
+        val texto_verde = findViewById<TextView>(R.id.texto_verde)
 
         val userDao = UserDataBase.getDataBase(application).userDao()
         val repository = UserRepository(userDao, Session.getIdUsuario())
         val viewModelFactory = UserViewModelFactory(repository,4)
         selCambiarFacViewModel = ViewModelProviders.of(this, viewModelFactory)
             .get(CambiarFaccionViewModel::class.java)
+
+        if(Language.idioma.equals("castellano")){
+            titulo.setText("¡CAMBIA DE EQUIPO!")
+            texto_amarillo.setText("¡La     guerra     y     la     paz     pueden     ir     de     la     mano     si     quieres     luchar     por     la     paz     de     toda     la     facción     amarilla     que     te     espera!")
+            texto_azul.setText("Todos     somos     iguales     pero     la     faccion     azul     lucha     hasta     que     ya     no     pueden.     ¡Unete     a     nosotros     y     lucha     por     una     Barcelona     mejor!")
+            texto_rojo.setText("Esto     no     es     un     juego,     es     una     confrontacion     real.     ¡Si     quieres     ganar,     unete     a     nosotros!")
+            texto_verde.setText("Juntos     dominaremos     toda     Barcelona     y     todas     las     facciones     sabran     que     el     verde     es     el     mejor.     ¡Ven     con     nosotros!")
+        }
+        else if(Language.idioma.equals("ingles")){
+            titulo.setText("CHANGE YOUR TEAM!")
+            texto_amarillo.setText("War     and     peace     can     go     hand     in     hand     if     u     want     to     fight     for     the     peace     of     all     the     yellow     faction     awaits     you!")
+            texto_azul.setText("We     are     all     the     same     but     the     blue     faction     fights     until     they     can     no     longer.     Join     us     and     fight     for     a     better     Barcelona!")
+            texto_rojo.setText("This     is     not     a     game,     this     is     a     real     confrontation.     If     you     wanna     win     then     join     us!")
+            texto_verde.setText("Together     we     will     dominate     all     of     Barcelona     and     all     factions     will     know     that     green     is     the     best     one.     Come     with     us!")
+        }
 
         selCambiarFacViewModel.responseChangeFaction.observe(this@CambiarFaccionActivity, Observer{ response ->
             if (response.isSuccessful){
--- app/src/main/java/com/example/runnerwar/ui/cuenta/CuentaFragment.kt
+++ app/src/main/java/com/example/runnerwar/ui/cuenta/CuentaFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -32,8 +33,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import io.getstream.chat.android.client.ChatClient
 import kotlinx.android.synthetic.main.fragment_cuenta.*
-import kotlinx.android.synthetic.main.fragment_cuenta.reg_email
-import kotlinx.android.synthetic.main.fragment_cuenta.reg_userName
 
 
 class CuentaFragment : Fragment() {
